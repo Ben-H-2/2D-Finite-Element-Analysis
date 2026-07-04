10f4d0ee17aa411fabbc6ea93ba1937437febfb4
Ben-H-2 <benjaminhorton141@gmail.com>
2026-07-04 21:10:59 +0000

    implement full pipeline

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 from fea.node import Node
 from fea.element import Element
-from fea.solver import create_global_matrix, build_force_vector, apply_boundary_conditions, solve_system
+from fea.solver import create_global_matrix, build_force_vector, apply_boundary_conditions, solve_system, expand_displacements
 import fea.visualisation
 
 def main():
-    n0 = Node(identifier=0, posx=0.0, is_fixed=True)
-    n1 = Node(identifier=1, posx=1.0)
-    n2 = Node(identifier=2, posx=2.0, force=10)
+    n0 = Node(identifier=0, posx=0.0, posy=0.0, is_fixed_x=True, is_fixed_y=True)
+    n1 = Node(identifier=1, posx=2.0, posy=0.0, is_fixed_x=True, is_fixed_y=True)
+    n2 = Node(identifier=2, posx=1.0, posy=1.5, force_y=-10)
 
-    e0 = Element(stiffness=5, leftnode=n0, rightnode=n1)
-    e1 = Element(stiffness=3, leftnode=n1, rightnode=n2)
+    nodes = [n0, n1, n2]
 
-    K = create_global_matrix([n0, n1, n2], [e0, e1])
-    F = build_force_vector([n0, n1, n2])
-    K_r, F_r = apply_boundary_conditions(K, F, [n0, n1, n2])
-    u = solve_system(K_r, F_r)
+    e0 = Element(E=200e9, A=0.001, leftnode=n0, rightnode=n1)
+    e1 = Element(E=200e9, A=0.001, leftnode=n0, rightnode=n2)
+    e2 = Element(E=200e9, A=0.001, leftnode=n1, rightnode=n2)
 
-    print(u)
+    elements = [e0, e1, e2]
 
+    K = create_global_matrix(nodes, elements)
+    F = build_force_vector(nodes)
+    K_r, F_r, remove = apply_boundary_conditions(K, F, nodes)
+    u_reduced = solve_system(K_r, F_r)
+    full_u = expand_displacements(u_reduced, remove, len(nodes)*2)
 
-
+    for element in elements:
+        stress = element.get_stress(full_u)
+        print(stress)
 
 if __name__ == "__main__":
     main()
