c2208a93c29c4f9342f91dd8c51715147cc3da62
Ben-H-2 <benjaminhorton141@gmail.com>
2026-07-05 17:38:48 +0000

    implement visuals for 2D

--- fea/visualisation.py
+++ fea/visualisation.py
@@ -1,3 +1,7 @@
+import pyvista as pv
+import numpy as np
+
+
 def build_node_index_map(nodes):
     node_to_index = {}
     for i, node in enumerate(nodes):
@@ -25,3 +29,31 @@ def build_points_array(nodes):
         coords = (node.posx, node.posy, 0)
         points.append(coords)
     return points
+
+def build_cells_array(elements, node_to_index):
+    cells_array = []
+    for element in elements:
+        cells_array.extend(get_cell_for_element(element,node_to_index))
+    return cells_array
+
+def render_mesh(nodes,elements):
+    mesh = pv.PolyData(np.array(build_points_array(nodes)), faces = np.array(build_cells_array(elements,build_node_index_map(nodes))))
+    return mesh
+
+if __name__ == "__main__":
+    from .node import Node
+    from .element import TriangleElement
+
+    n0 = Node(identifier=0, posx=0.0, posy=0.0, is_fixed_x=True, is_fixed_y=True)
+    n1 = Node(identifier=1, posx=3.0, posy=0.0, is_fixed_x=True, is_fixed_y=True)
+    n2 = Node(identifier=2, posx=1.0, posy=4.0, force_x=1000, force_y=-500)
+
+    nodes = [n0, n1, n2]
+
+    tri = TriangleElement(E=200e9, nu=0.3, thickness=0.01, node_a=n0, node_b=n1, node_c=n2)
+    elements = [tri]
+
+    mesh = render_mesh(nodes, elements)
+    mesh.plot()
+    print(mesh)
+
