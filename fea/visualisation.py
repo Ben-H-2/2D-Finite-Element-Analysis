9722b9fcb0599b99c3f922b914570e48dc745ab5
Ben-H-2 <benjaminhorton141@gmail.com>
2026-07-05 20:18:29 +0000

    add von mises stress function

--- fea/visualisation.py
+++ fea/visualisation.py
@@ -36,24 +36,78 @@ def build_cells_array(elements, node_to_index):
         cells_array.extend(get_cell_for_element(element,node_to_index))
     return cells_array
 
-def render_mesh(nodes,elements):
-    mesh = pv.PolyData(np.array(build_points_array(nodes)), faces = np.array(build_cells_array(elements,build_node_index_map(nodes))))
+def get_deformed_position(node, full_u,scale=1):
+    x_disp = scale*(full_u[node.identifier*2])
+    y_disp = scale*(full_u[node.identifier*2+1])
+    return (node.posx+x_disp, node.posy+y_disp, 0)
+
+def build_deformed_points_array(nodes,full_u,scale=1):
+    points = []
+    for node in nodes:
+        coords = get_deformed_position(node,full_u,scale)
+        points.append(coords)
+    return points
+
+def build_stress_array(elements, full_u):
+    von_mises_stress_list = []
+    for element in elements:
+        von_mises_stress = element.get_von_mises_stress(full_u)
+        von_mises_stress_list.append(von_mises_stress)
+    return von_mises_stress_list
+
+def render_mesh(nodes,elements,full_u=None,scale=1):
+    if full_u is None:
+        points = build_points_array(nodes)
+    else:
+        points = build_deformed_points_array(nodes,full_u,scale)
+    node_to_index = build_node_index_map(nodes)
+    cells = build_cells_array(elements,node_to_index)
+
+    points_array = np.array(points)
+    cells_array = np.array(cells)
+
+    mesh = pv.PolyData(points_array, faces = cells_array)
     return mesh
+
+def show_mesh(mesh, show_edges=True, edge_color="black", line_width=3):
+    plotter = pv.Plotter()
+    plotter.add_mesh(mesh, show_edges=show_edges, edge_color=edge_color, line_width=line_width)
+    plotter.add_text("W: wireframe   S: surface", font_size=10)
+    plotter.show()
+
+
 
 if __name__ == "__main__":
     from .node import Node
-    from .element import TriangleElement
+    from .element import Element
+    from .solver import (
+        create_global_matrix,
+        build_force_vector,
+        apply_boundary_conditions,
+        solve_system,
+        expand_displacements,
+    )
 
     n0 = Node(identifier=0, posx=0.0, posy=0.0, is_fixed_x=True, is_fixed_y=True)
-    n1 = Node(identifier=1, posx=3.0, posy=0.0, is_fixed_x=True, is_fixed_y=True)
-    n2 = Node(identifier=2, posx=1.0, posy=4.0, force_x=1000, force_y=-500)
+    n1 = Node(identifier=1, posx=2.0, posy=0.0, is_fixed_x=True, is_fixed_y=True)
+    n2 = Node(identifier=2, posx=1.0, posy=1.5, force_y=-10)
 
     nodes = [n0, n1, n2]
 
-    tri = TriangleElement(E=200e9, nu=0.3, thickness=0.01, node_a=n0, node_b=n1, node_c=n2)
-    elements = [tri]
+    e0 = Element(E=200e9, A=0.001, leftnode=n0, rightnode=n1)
+    e1 = Element(E=200e9, A=0.001, leftnode=n0, rightnode=n2)
+    e2 = Element(E=200e9, A=0.001, leftnode=n1, rightnode=n2)
 
-    mesh = render_mesh(nodes, elements)
-    mesh.plot()
+    elements = [e0, e1, e2]
+
+    K = create_global_matrix(nodes, elements)
+    F = build_force_vector(nodes)
+    K_r, F_r, remove = apply_boundary_conditions(K, F, nodes)
+    u_reduced = solve_system(K_r, F_r)
+    full_u = expand_displacements(u_reduced, remove, len(nodes) * 2)
+    print(full_u)
+
+    mesh = render_mesh(nodes, elements, full_u,scale=10000000)
     print(mesh)
+    show_mesh(mesh)
 
