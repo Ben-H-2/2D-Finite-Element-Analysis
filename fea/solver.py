32a0a459e29f723f7d043ad52dda57db4bbb6565
Ben-H-2 <benjaminhorton141@gmail.com>
2026-07-06 10:21:17 +0000

    sparse matrix refactor

--- fea/solver.py
+++ fea/solver.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.sparse import coo_matrix
+from scipy.sparse.linalg import spsolve
 
 def get_dofs(nodes_list):
     dofs = []
@@ -9,12 +11,19 @@ def get_dofs(nodes_list):
 
 def create_global_matrix(nodes, elements):
     n = len(nodes)*2
-    K = np.zeros((n, n))
+    row_list = []
+    col_list = []
+    value_list = []
     for element in elements:
         local_matrix = element.create_stiffness_matrix()
         node_list = element.get_nodes()
         dofs = get_dofs(node_list)
-        K[np.ix_(dofs, dofs)] += local_matrix
+        for i in range(len(dofs)):
+            for j in range(len(dofs)):
+                value_list.append(local_matrix[i,j])
+                row_list.append(dofs[i])
+                col_list.append(dofs[j])
+    K = coo_matrix((value_list, (row_list, col_list)), shape=(n,n))
     return K
 
 def build_force_vector(nodes):
@@ -25,19 +34,21 @@ def build_force_vector(nodes):
     return F
 
 def apply_boundary_conditions(K, F, nodes):
+    n = len(nodes)*2
     remove = []
     for node in nodes:
-        if node.is_fixed_x == True:
+        if node.is_fixed_x:
             remove.append(node.identifier*2)
-        if node.is_fixed_y == True:
+        if node.is_fixed_y:
             remove.append(node.identifier*2+1)
-    K = np.delete(K,remove,axis=1)
-    K = np.delete(K,remove,axis=0)
-    F = np.delete(F,remove,axis=0)       
-    return K, F, remove
+    keep = [dof for dof in range(n) if dof not in remove]
+    K_r = K.tocsr()[keep, :][:, keep] #Trims unnecessary columns and rows
+    F_r = F[keep]
+           
+    return K_r, F_r, remove
 
 def solve_system(K_reduced, F_reduced):
-    return(np.linalg.solve(K_reduced,F_reduced))
+    return spsolve(K_reduced, F_reduced)
 
 def expand_displacements(u_reduced, remove, total_dofs):
     full_u = np.zeros(total_dofs)
