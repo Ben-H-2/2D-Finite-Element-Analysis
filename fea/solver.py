565f91d113e6251cfed97ed7e5eeb33f45cb878f
Ben-H-2 <benjaminhorton141@gmail.com>
2026-07-07 20:31:49 +0000

    replace node.identifier with node_index to allow node removal

--- fea/solver.py
+++ fea/solver.py
@@ -2,50 +2,49 @@ import numpy as np
 from scipy.sparse import coo_matrix
 from scipy.sparse.linalg import spsolve
 
-def get_dofs(nodes_list):
+def build_node_index(nodes):
+    return {node: i for i, node in enumerate(nodes)}
+
+def get_dofs(nodes_list, node_index):
     dofs = []
     for node in nodes_list:
-        dofs.append(node.identifier*2)
-        dofs.append(node.identifier*2+1)
+        idx = node_index[node]
+        dofs.append(idx*2)
+        dofs.append(idx*2+1)
     return dofs
 
-def create_global_matrix(nodes, elements):
+def create_global_matrix(nodes, elements, node_index):
     n = len(nodes)*2
-    row_list = []
-    col_list = []
-    value_list = []
+    row_list, col_list, value_list = [], [], []
     for element in elements:
         local_matrix = element.create_stiffness_matrix()
-        node_list = element.get_nodes()
-        dofs = get_dofs(node_list)
+        dofs = get_dofs(element.get_nodes(), node_index)
         for i in range(len(dofs)):
             for j in range(len(dofs)):
                 value_list.append(local_matrix[i,j])
                 row_list.append(dofs[i])
                 col_list.append(dofs[j])
-    K = coo_matrix((value_list, (row_list, col_list)), shape=(n,n))
-    return K
+    return coo_matrix((value_list, (row_list, col_list)), shape=(n,n))
 
-def build_force_vector(nodes):
-    F = np.zeros((len(nodes)*2))
+def build_force_vector(nodes, node_index):
+    F = np.zeros(len(nodes)*2)
     for node in nodes:
-        F[node.identifier*2] = node.force_x
-        F[node.identifier*2+1] = node.force_y
+        idx = node_index[node]
+        F[idx*2] = node.force_x
+        F[idx*2+1] = node.force_y
     return F
 
-def apply_boundary_conditions(K, F, nodes):
+def apply_boundary_conditions(K, F, nodes, node_index):
     n = len(nodes)*2
     remove = []
     for node in nodes:
-        if node.is_fixed_x:
-            remove.append(node.identifier*2)
-        if node.is_fixed_y:
-            remove.append(node.identifier*2+1)
+        idx = node_index[node]
+        if node.is_fixed_x: remove.append(idx*2)
+        if node.is_fixed_y: remove.append(idx*2+1)
     remove_set = set(remove)
     keep = [dof for dof in range(n) if dof not in remove_set]
-    K_r = K.tocsr()[keep, :][:, keep] #Trims unnecessary columns and rows
+    K_r = K.tocsr()[keep, :][:, keep]
     F_r = F[keep]
-           
     return K_r, F_r, remove
 
 def solve_system(K_reduced, F_reduced):
