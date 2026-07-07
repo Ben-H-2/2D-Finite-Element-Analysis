18b718ca58f6625f24f476ce474eb03057ee013b
Ben-H-2 <benjaminhorton141@gmail.com>
2026-07-07 21:49:37 +0000

    clear redundant leftover code

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fea.node import Node
 from fea.element import Element
-from fea.solver import create_global_matrix, build_force_vector, apply_boundary_conditions, solve_system, expand_displacements
+from fea.solver import create_global_matrix, build_force_vector, reduce_system, solve_system, expand_displacements
 import fea.visualisation
 
 def main():
-
+    pass
 if __name__ == "__main__":
     main()
